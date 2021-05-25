a6c9ce622aad90d8c5d6dc50e30f10904d31fe99
tonglei1124 <41976963+tonglei1124@users.noreply.github.com>
2021-05-25 23:55:25 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::net::TcpListener;
 use std::fs;
 
 fn main() {
-    // 创建TCP连接 监听本地7878端口
+    // 创建TCP连接
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    //循环获取流数据
+    // 获取流数据
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         // 流处理方法
@@ -16,16 +16,16 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    //创建1024字节缓冲区
+    // 创建1024字节缓冲区
     let mut buffer = [0; 1024];
     let resut = stream.read(&mut buffer);
-    //模式匹配读取的结果
+    // 模式匹配读取的结果
     match resut {
-        //读取正常
+        // 读取正常
         Ok(_) => {
             println!("input stream is  ok!");
         }
-        //读取有误，panic退出
+        // 读取有误，panic退出
         Err(error) => {
             println!("input stream is error!");
             panic!("Problem opening the file: {:?}", error)
@@ -33,22 +33,22 @@ fn handle_connection(mut stream: TcpStream) {
     }
 
 
-    //打印接受的信息
+    // 打印信息
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    // 定义字节数组
+    // 定义数组
     let get = b"GET / HTTP/1.1\r\n";
-    //定义tatus_lien和返回文件名
+    // 定义tatus_lien并返回文件名
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
-    //读取文件名称
+    // 读取文件
     let contents = fs::read_to_string(filename).unwrap();
-    //设置文件头
+    // 设置文件title
     let response = format!("{}{}", status_line, contents);
-    //返回内容html给调用客户端
+    // 返回内容
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
